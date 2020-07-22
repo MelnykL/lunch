23c2566d879eba4f021d19d5c4d2c37d033134d9
MelnykL <elem.ukr@gmail.com>
2020-07-22 10:21:46 +0300

    fix orders for production

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   load_and_authorize_resource
   
   def index
-    @q = Order.includes(:user).group(:created_at).ransack(params[:q])
+    @q = Order.includes(:user).order(created_at: :desc).ransack(params[:q])
     @orders = @q.result
     @todays_orders = Order.includes(:user).where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).all
   end
